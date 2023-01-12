9c3a6951cabea9b5463dd3a9caae639b3f21e0fc
RafaelMorais <rafaelmorais.design@gmail.com>
2023-01-12 20:03:57 -0300

    Exercício de Polimorfismo

---
+++ FormaGeometrica.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    public class FormaGeometrica
+    {
+        public virtual void Desenhar()
+        {
+            Console.WriteLine("Desenhando uma forma");
+        }
+    }
+
+    public class Circulo : FormaGeometrica
+    {
+        public override void Desenhar()
+        {
+            Console.WriteLine("Desenhando um circulo");
+        }
+    }
+
+    public class Quadrado : FormaGeometrica
+    {
+        public override void Desenhar()
+        {
+            Console.WriteLine("Desenhando um quadrado");
+        }
+    }
+}
